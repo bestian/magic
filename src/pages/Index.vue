78a809804fc0e2a195dea8a4d7c259a622f46404
Bestian <bestian@gmail.com>
2021-06-18 16:35:10 +0800

    small

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,10 @@
   	<h3>古典迫牌</h3>
   	<br/>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/yoaeKKNDW5Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <br/>
+  	<h3>撥牌迫牌</h3>
+  	<br/>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/6M77IE4eiwU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </q-page>
 </template>
 
