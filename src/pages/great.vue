f115a6c7c23d256c382dc66ae7ae8bf17fa1a87a
Bestian <bestian@gmail.com>
2021-06-18 16:43:08 +0800

    add great

---
+++ src/pages/great.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="container">
+  	<h3>劉謙-巴格拉斯效果</h3>
+  	<br/>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/iaJW1dVxOfk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style type="text/css" scoped="">
+	.container {
+		max-width: 800px;
+		margin: 0 auto;
+	}
+</style>
