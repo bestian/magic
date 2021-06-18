589ce9242e5d4abdf6708fa983a2bc595e652480
Bestian <bestian@gmail.com>
2021-06-18 15:57:52 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-quasar build -m pwa --modern
+quasar build -m pwa
 
 # navigate into the build output directory
 cd dist/pwa
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:3dw/q-words.git master:gh-pages
+git push -f git@github.com:bestian/magic.git master:gh-pages
 
 cd -
