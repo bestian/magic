aa0404a61156597254ee3a5d9d8fac9f62d882b4
Bestian Tang 小巴 <bestian@gmail.com>
2021-06-20 19:43:56 +0800

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,42 @@
 <template>
   <q-page class="container">
-    <h3>4A出現</h3>
-    <br/>
-    <video width="560" height="315" :src="'./4A出現.mp4'" title="4A出現" controls=""></video>
-  	<h3>古典迫牌</h3>
-  	<br/>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/yoaeKKNDW5Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <br/>
-  	<h3>撥牌迫牌</h3>
-  	<br/>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/6M77IE4eiwU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <q-input type="text" name="s" v-model="s" placeholder="關鍵字查詢" />
+    <div v-for="item in search(items, s)" :key="item.n">
+      <h3>{{item.n}}</h3>
+      <br/>
+      <video v-if ="item.src" width="560" height="315" :src="item.src" :title="item.n" controls=""></video>
+      <iframe v-if="item.iframe" width="560" height="315" :src="item.iframe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      s: null,
+      items: [
+        {
+          n: '4A出現',
+          src: './4A出現.mp4'
+        },
+        {
+          n: '古典迫牌',
+          iframe: 'https://www.youtube.com/embed/yoaeKKNDW5Y'
+        },
+        {
+          n: '撥牌迫牌',
+          iframe: 'https://www.youtube.com/embed/6M77IE4eiwU'
+        }
+      ]
+    }
+  },
+  methods: {
+    search(list, k){
+      return list.filter((o) => !k || o.n.indexOf(k) > -1)
+    }
+  }
 }
 </script>
 
