a4300c1dd1e152042c34eaaedecc241364671a0a
Bestian Tang <bestian@gmail.com>
2021-06-20 16:37:22 +0800

    add item

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,8 @@
 <template>
   <q-page class="container">
+    <h3>4A出現</h3>
+    <br/>
+    <video width="560" height="315" :src="'./4A出現.mp4'" title="4A出現" controls=""></video>
   	<h3>古典迫牌</h3>
   	<br/>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/yoaeKKNDW5Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
